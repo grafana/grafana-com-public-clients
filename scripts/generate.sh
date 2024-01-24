1f899b40f46e5b0acdd12a1f0706cd37c1d494aa
Julien Duchesne <julien.duchesne@grafana.com>
2024-01-24 10:50:18 -0500

    Run goimports on output

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -eo pipefail
 
+wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.2.0/openapi-generator-cli-7.2.0.jar -O ./openapi-generator-cli.jar
 
-echo "Generating GO Client"
 # Cleanup old files
 rm -rf ./go/gcom
-docker run --rm \
-  -v "${PWD}":/local openapitools/openapi-generator-cli generate \
-  -i /local/openapi.json \
+
+
+java -jar openapi-generator-cli.jar generate \
+  -i openapi.json \
   -g go \
-  -o /local/go/gcom \
+  -o ./go/gcom \
   --git-user-id grafana \
   --git-repo-id grafana-com-public-clients/go \
   -p packageName=gcom \
   -p isGoSubmodule=true
+
+if ! command -v goimports &> /dev/null
+then
+    go install golang.org/x/tools/cmd/goimports@latest
+fi
+find ./go/gcom -name \*.go -exec goimports -w {} \;
 
