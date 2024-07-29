da229f369a57cc2b87deab31c484b62493d98325
Ryan Melendez <rmelend1@kent.edu>
2024-07-29 09:26:56 -0400

    Also fix list response

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,6 +12,7 @@ modify() {
     SCHEMA="$(echo "${SCHEMA}" | jq "${1}")"
 }
 modify '.components.schemas.FormattedApiApiKey.properties.id = { "anyOf": [ { "type": "string" }, { "type": "number" } ] }'
+modify '.components.schemas.FormattedApiApiKeyListResponse.properties.items.items.properties.id = { "anyOf": [ { "type": "string" }, { "type": "number" } ] }'
 modify '.components.schemas.FormattedOrgMembership.properties.allowGCloudTrial = { "anyOf": [ { "type": "boolean" }, { "type": "number" } ] }'
 modify '.components.schemas.FormattedApiOrgPublic.properties.allowGCloudTrial = { "anyOf": [ { "type": "boolean" }, { "type": "number" } ] }'
 modify '.paths["/v1/accesspolicies"].get.responses["200"].content["application/json"].schema = {
