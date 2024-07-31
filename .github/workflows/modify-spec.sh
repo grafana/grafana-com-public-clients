aeaa710d12587abdc2c600d17a016e9dbc487f57
Julien Duchesne <julien.duchesne@grafana.com>
2024-07-31 11:36:43 -0400

    Use new action to generate client

--- .github/workflows/modify-spec.sh
+++ .github/workflows/modify-spec.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
-set -eo pipefail
+#! /usr/bin/env bash
+set -euo pipefail
 
-wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.2.0/openapi-generator-cli-7.2.0.jar -O ./openapi-generator-cli.jar
-
-# Cleanup old files
-rm -rf ./go/gcom
-
-# Workarounds for temporary schema issues
-SCHEMA="$(cat openapi.json)"
+SCHEMA=`cat` # Read stdin
 modify() {
     SCHEMA="$(echo "${SCHEMA}" | jq "${1}")"
 }
@@ -27,25 +21,4 @@ modify '.paths["/v1/accesspolicies"].get.responses["200"].content["application/j
   }
 }'
 
-TEMP_OPENAPI_FILE="openapi-temp.json"
-echo "${SCHEMA}" > $TEMP_OPENAPI_FILE
-trap "rm -f $TEMP_OPENAPI_FILE" EXIT
-
-
-java -jar openapi-generator-cli.jar generate \
-  -i ${TEMP_OPENAPI_FILE} \
-  -g go \
-  -o ./go/gcom \
-  --git-user-id grafana \
-  --git-repo-id grafana-com-public-clients/go \
-  -p packageName=gcom \
-  -p isGoSubmodule=true \
-  -p disallowAdditionalPropertiesIfNotPresent=false \
-  -t ./scripts
-
-if ! command -v goimports &> /dev/null
-then
-    go install golang.org/x/tools/cmd/goimports@latest
-fi
-find ./go/gcom -name \*.go -exec goimports -w {} \;
-
+echo "${SCHEMA}"
